32835876e7b9c90e2a0dcd8cbc9303ae81598513
Kevin Do <do.kevin.vo@gmail.com>
2018-09-03 19:54:27 -0700

    v0.3: Getting somewhere

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
     $("#submitBtn").on("click", function () {
         var apiKey = "&api_key=" + "KYDe3oh6tBmq8a7wwvgEYIycs35ANaL0";
         var queryTerm = "&q=" + $("#userInput").val();
-        var queryURL = "http://api.giphy.com/v1/gifs/search?&limit=10" + queryTerm + apiKey;
+        var queryURL = "http://api.giphy.com/v1/gifs/search?" + "&limit=10" + queryTerm + apiKey;
         console.log(queryURL);
 
         $.ajax({
@@ -26,11 +26,18 @@ $(document).ready(function () {
         }).then(function(response) {
 
             for (var i = 0; i < response.data.length; i++) {
-                var gifURL = response.data[i].images.original.url;
+                var gifURL = response.data[i].images.downsized.url;
+                var gifRating = response.data[i].rating;
+                console.log(gifRating);
             
-                var topicGIF = $("<img>");
-    
-                topicGIF.attr("src", gifURL).attr("alt", searchTerm);
+                var topicGIF = $("<img>" + "<p>Rating: " + gifRating + "</p>");
+                    topicGIF.attr("src", gifURL).attr("alt", queryTerm);
+                    topicGIF.before('<div class="col-md-4">').after("</div>");
+                // $(topicGIF).before('<div class="col-md-4">').after("</div>");
+
+                // var gifStillURL = response.data[i].images.downsized_still.url;
+                // topicGif.attr("src", gifStillURL).attr("data-still", gifStillURL);
+
     
                 $(".topicGIFs").prepend(topicGIF);
             }
